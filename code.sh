fdbbe6088b0849afaf228b6c232f4d516eacbc26
Tony3201 <Tony3201@users.noreply.github.com>
2021-08-26 13:19:54 -0400

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+#执行程序
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "本次运行时间： "$((end_seconds-start_seconds))"s"
 
